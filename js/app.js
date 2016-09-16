5a19011344d1db9791e901948b29affa0bc1479e
lili hualtibamba <lilihperezz@gmail.com>
2016-09-16 11:18:24 -0500

    cri

--- js/app.js
+++ js/app.js
@@ -6,24 +6,44 @@ window.addEventListener("load", function() {
     var color = document.getElementById("color").value;
     if (color=="celeste"){
         document.getElementById("circulo1").style.border = "2px solid black";
-        document.getElementById("circulo2").style.border = "0px solid black";
-        document.getElementById("circulo3").style.border = "0px solid black";
+        document.getElementById("circulo2").style.border = "transparent";
+        document.getElementById("circulo3").style.border = "transparent";
     } else if (color=="rojo"){
-        document.getElementById("circulo1").style.border = "0px solid black";
+        document.getElementById("circulo1").style.border = "transparent";
         document.getElementById("circulo2").style.border = "2px solid black";
-        document.getElementById("circulo3").style.border = "0px solid black";
+        document.getElementById("circulo3").style.border = "transparent";
     } else if (color=="anaranjado"){
-        document.getElementById("circulo1").style.border = "0px solid black";
-        document.getElementById("circulo2").style.border = "0px solid black";
+        document.getElementById("circulo1").style.border = "transparent";
+        document.getElementById("circulo2").style.border = "transparent";
         document.getElementById("circulo3").style.border = "2px solid black";
     }
         
     });
-});
+
 //cri
-/*
     var boton = document.getElementById("cri");
     var conta = 1;
-    boton.addEventListener("click", function() {
-     */
+    boton.addEventListener("click", function() {    
+       if(conta == 1){
+            document.getElementById("circulo1").style.border = "2px solid green";
+            document.getElementById("color").placeholder = "celeste";
+            document.getElementById("circulo2").style.borderColor = "transparent";
+            document.getElementById("circulo3").style.borderColor = "transparent";
+        }
+       if(conta == 2){
+            document.getElementById("circulo2").style.border = "2px solid green";
+            document.getElementById("color").placeholder = "rojo";
+            document.getElementById("circulo1").style.borderColor = "transparent";
+            document.getElementById("circulo3").style.borderColor = "transparent";
+        }
+        if(conta == 3){
+            document.getElementById("circulo3").style.border = "2px solid green";
+            document.getElementById("color").placeholder = "anaranjado";
+            document.getElementById("circulo2").style.borderColor = "transparent";
+            document.getElementById("circulo1").style.borderColor = "transparent";
+            conta = 0;
+        }
+        conta++;
+    });
+ });
         
